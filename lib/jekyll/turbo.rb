2d87cd72ae74ccf330a3f5f7fb5bce1161b288c3
maikhel <mlecicki@gmail.com>
2025-05-02 21:57:24 +0200

    Refactor Turbo script injection and add tests

--- lib/jekyll/turbo.rb
+++ lib/jekyll/turbo.rb
@@ -17,16 +17,20 @@ module Jekyll
     class Injector
       CDN_URL = "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.4/dist/turbo.es2017-esm.js"
 
+      def self.add_script(page)
+        return unless page.output_ext == ".html" && page.output.include?("</head>")
+
+        turbo_script = <<~HTML
+          <script type="module" src="#{CDN_URL}"></script>
+        HTML
+
+        # Inject the Turbo script just before </head>
+        page.output = page.output.sub("</head>", "#{turbo_script}\n</head>")
+      end
+
       # Hook into the post_render step for each page
       Jekyll::Hooks.register :pages, :post_render do |page|
-        if page.output_ext == ".html" && page.output.include?("</head>")
-          turbo_script = <<~HTML
-            <script type="module" src="#{CDN_URL}"></script>
-          HTML
-
-          # Inject the Turbo script just before </head>
-          page.output.gsub!("</head>", "#{turbo_script}\n</head>")
-        end
+        Jekyll::Turbo::Inject.add_script(page)
       end
     end
   end
--- test/jekyll/test_turbo.rb
+++ test/jekyll/test_turbo.rb
@@ -4,12 +4,48 @@ require "test_helper"
 
 module Jekyll
   class TestTurbo < Minitest::Test
+    TestPage = Struct.new(:output_ext, :output)
+
+    def setup # rubocop:disable Metrics/MethodLength
+      @html_input = <<~HTML
+        <html>
+          <head>
+            <title>Test</title>
+          </head>
+          <body>
+            <p>Hello World</p>
+          </body>
+        </html>
+      HTML
+
+      @expected_script = '<script type="module" src="https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.4/dist/turbo.es2017-esm.js"></script>'
+    end
+
     def test_that_it_has_a_version_number
       refute_nil ::Jekyll::Turbo::VERSION
     end
 
-    def test_it_does_something_useful
-      assert 1, 1
+    def test_is_not_injected_into_non_html
+      page = TestPage.new(".xml", @html_input)
+      output = Jekyll::Turbo::Injector.add_script(page)
+
+      assert_nil output
+    end
+
+    def test_script_is_injected_into_head
+      page = TestPage.new(".html", @html_input)
+      output = Jekyll::Turbo::Injector.add_script(page)
+
+      assert_includes output, @expected_script
+    end
+
+    def test_script_comes_after_opening_head_tag
+      page = TestPage.new(".html", @html_input)
+      output = Jekyll::Turbo::Injector.add_script(page)
+      index_of_head = output.index("<head>") + "<head>".length
+      index_of_script = output.index(@expected_script)
+
+      assert index_of_script > index_of_head, "Script should be injected right after <head>"
     end
   end
 end
